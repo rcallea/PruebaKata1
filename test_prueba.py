fa63ec74cb43fbf65ea78a1c94aaa7bde5e66af9
cr.calle <cr.calle@uniandes.edu.co>
2018-09-21 19:44:29 -0500

    Paso5: Divisible por 5 (Rojo)

--- test_prueba.py
+++ test_prueba.py
@@ -7,3 +7,8 @@ class TestPrueba(TestCase):
         prueba = Prueba()
         self.assertEqual(prueba.fizzBuzz(3),"fizz")
         self.assertNotEqual(prueba.fizzBuzz(5),"fizz")
+
+    def test_buzz(self):
+        prueba = Prueba()
+        self.assertEqual(prueba.fizzBuzz(5),"buzz")
+        self.assertNotEqual(prueba.fizzBuzz(3),"buzz")
