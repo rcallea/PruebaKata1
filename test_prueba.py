0b3d15497da6673363c2dcd048559fd1332f4831
cr.calle <cr.calle@uniandes.edu.co>
2018-09-21 19:33:01 -0500

    Paso 3: Test con 5 no divisible por tres (Rojo)

--- test_prueba.py
+++ test_prueba.py
@@ -6,3 +6,4 @@ class TestPrueba(TestCase):
     def test_fizz(self):
         prueba = Prueba()
         self.assertEqual(prueba.fizzBuzz(3),"fizz")
+        self.assertNotEqual(prueba.fizzBuzz(5),"fizz")
